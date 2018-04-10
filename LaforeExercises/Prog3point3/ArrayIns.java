8ba1067f3165d6f20e66627df8d00cd4b11ce80b
Gaureesh Anvekar <gaureeshanvekar@gmail.com>
2018-04-11 00:06:26 +0530

    Program 3.3

---
+++ LaforeExercises/Prog3point3/ArrayIns.java
@@ -0,0 +1,63 @@
+package Prog3point3;
+
+/**
+ * Created by gaureesh on 13/7/17.
+ */
+public class ArrayIns {
+    private long[] a;                 // ref to array a
+    private int nElems;               // number of data items
+    //--------------------------------------------------------------
+    public ArrayIns(int max)          // constructor
+    {
+        a = new long[max];                 // create the array
+        nElems = 0;                        // no items yet
+    }
+    //--------------------------------------------------------------
+    public void insert(long value)    // put element into array
+    {
+        a[nElems] = value;             // insert it
+        nElems++;                      // increment size
+    }
+    //--------------------------------------------------------------
+    public void display()             // displays array contents
+    {
+        for(int j=0; j<nElems; j++)       // for each element,
+            System.out.print(a[j] + " ");  // display it
+        System.out.println("");
+    }
+    //--------------------------------------------------------------
+    public void insertionSort()
+    {
+        int in, out;
+
+        for(out=1; out<nElems; out++)     // out is dividing line
+        {
+            long temp = a[out];            // remove marked item
+            in = out;                      // start shifts at out
+            while(in>0 && a[in-1] >= temp) // until one is smaller,
+            {
+                a[in] = a[in-1];            // shift item to right
+                --in;                       // go left one position
+            }
+            a[in] = temp;                  // insert marked item
+        }  // end for
+    }  // end insertionSort()
+
+    //no dups which is O(n)
+    public void noDups() {
+        this.insertionSort();
+        int count = 0;
+        for( int i = 0; i < this.nElems; i++ ) {
+            if( this.a[i] == this.a[i+1] ) {
+                count++;
+            } else if( count > 0 ) {
+                for( int shift = i+1; shift < this.nElems; shift++ ) {
+                    this.a[shift-count] = this.a[shift];
+                }
+                this.nElems = this.nElems - count;
+                i = (i+1) - count;
+                count = 0;
+            }
+        }
+    }
+}
